3b010aeb7eafac7df01da02491445bd9cb04cc9d
nicolaazonta <nicolazonta114@gmail.com>
2023-03-13 21:21:11 +0100

    complete dice virtual game

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,6 +19,7 @@ let writtenName;
 nameButtonEl.addEventListener('click' , function (){
     //console.log(guestNameEl.value);
     writtenName = guestNameEl.value;
+    document.querySelector('h3').innerHTML=''
     //console.log(writtenName);
 
 //verificare se l'utente è nella lista 
@@ -29,13 +30,36 @@ nameButtonEl.addEventListener('click' , function (){
         //console.log(listName);
     
         if(listName === writtenName){
-            document.querySelector('h2').innerHTML='Congrats, you are in the guest list';
+            document.querySelector('h2').innerHTML='Congrats, you are in the guest list';    
         } else {
             document.querySelector('h2').innerHTML='Sorry, you are not in the guest list';
         }
     }
 }
 );
+
+/* Gioco dei dadi
+Generare un numero random da 1 a 6, sia per il giocatore sia per il computer. Stabilire il vincitore, in base a chi fa il punteggio più alto.*/
+const diceButtonEl = document.getElementById('dice_button');
+
+diceButtonEl.addEventListener('click' , function (){
+    const pcDice = (Math.random() * (6) + 1).toFixed(0) ;
+    const userDice = (Math.random() * (6) + 1).toFixed(0) ;
+
+
+
+    //Min + (int)(Math.random() * ((Max - Min) + 1));
+
+
+    if(pcDice > userDice){
+        document.querySelector('h4').innerHTML='You lose';
+    } else if(userDice > pcDice){
+        document.querySelector('h4').innerHTML='You win';        
+    } else {
+        document.querySelector('h4').innerHTML='Parity, no one win'; 
+    }
+}
+)
 
 
 
